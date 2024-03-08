17e3b751fa860a8f1a47e9f9f41aac10c07d4d16
Wellington Pereira <wellsouza24@gmail.com>
2024-03-07 21:26:48 -0300

    :lipstick: feat: Loading & Empty states  - Loading dos pedidos

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -15,6 +15,7 @@ import { useQuery } from "@tanstack/react-query";
 import { getOrders } from "@/api/get-orders";
 import { useSearchParams } from "react-router-dom";
 import { z } from "zod";
+import { OrderTableSkeleton } from "./order-table-skeleton";
 
 export function Orders() {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -28,7 +29,7 @@ export function Orders() {
     .transform((page) => page - 1)
     .parse(searchParams.get("page") ?? "1");
 
-  const { data: result } = useQuery({
+  const { data: result, isLoading: isLoadingOrders } = useQuery({
     queryKey: ["orders", pageIndex, orderId, customerName, status],
     queryFn: () =>
       getOrders({
@@ -78,6 +79,7 @@ export function Orders() {
               </TableBody>
             </Table>
           </div>
+          {isLoadingOrders && <OrderTableSkeleton />}
           {result && (
             <Pagination
               onPageChange={handlePaginate}
